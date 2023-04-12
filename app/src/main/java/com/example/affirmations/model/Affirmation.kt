2996d4c2e293b3db9c7426eda17ba72d065ca11b
Pjcel11 <pierrejean.martin.pjm@gmail.com>
2023-04-12 13:26:19 -0400

    Changed Affirmation.kt and Datasource.kt to accept the images

--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,4 +1,11 @@
 package com.example.affirmations.model
 // pas compris a quoi ca sert : plus ou moins importer les strings depuis les ressources
-data class Affirmation(val stringResourceId: Int) {
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+){
+
 }
