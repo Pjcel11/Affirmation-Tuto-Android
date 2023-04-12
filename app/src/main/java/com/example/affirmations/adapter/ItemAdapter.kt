a6b7271a61061b05f31cc73e5af0314e955e831c
Pjcel11 <pierrejean.martin.pjm@gmail.com>
2023-04-12 13:33:37 -0400

    Update of the ItemViewHolder to accept images

--- app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.R
@@ -22,9 +23,11 @@ class ItemAdapter (
     // you provide access to all the views for a data item in a view holder.
     // Each data item is just an Affirmation object.
 
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+    class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
+
 
     /**
      * Create new views (invoked by the layout manager)
